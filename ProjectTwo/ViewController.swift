26e595f76b8c8d22e7bc11757802e2a4ce4bf303
Lincoln Stewart <dvrkrvin@gmail.com>
2022-10-10 13:44:41 -0600

    Initial Commit

---
+++ ProjectTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectTwo
+//
+//  Created by Lincoln Stewart on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectTwoUITests/ProjectTwoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProjectTwoUITestsLaunchTests.swift
+//  ProjectTwoUITests
+//
+//  Created by Lincoln Stewart on 10/10/22.
+//
+
+import XCTest
+
+class ProjectTwoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
